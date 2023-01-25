63910fbc658957285bf470822c36f5dc2984ae99
Mohit Choyal <72176576+mohitchoyal2002@users.noreply.github.com>
2023-01-25 10:59:52 +0530

    Add: Routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import styled from 'styled-components';
 import './App.css';
-
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Products from './Components/Products'
+import { Routes, Route} from 'react-router-dom'
+import Resources from './Components/Resources'
+import KnowledgeBase from './Components/KnowledgeBase'
 function App() {
   return (
-    <div></div>
+    <Wrap>
+        <Header/>
+        <Routes>
+          <Route path='' element={<Home/>}/>
+          <Route path='/products' element={<Products/>}/>
+          <Route path='/resources' element={<Resources/>}/>
+          <Route path='/knowledge-base' element = {<KnowledgeBase/>}/>
+        </Routes>
+    </Wrap>
   );
 }
 
 export default App;
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+`
